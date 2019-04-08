36bd5b42371a79e171d2decc55ceda4e140571f1
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-04-08 21:17:52 +0600

    Generics

---
+++ Generics/Drive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Drive : SystemEntity, IData
+    {
+            public string PrintInfo()
+        {
+            return "Информация о диске";
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generics нужен для того чтобы сократить код и обращаться к любому значению 
+            //С помощью generics можно менять тип на момент его создания
+
+            int[] numbersAsArray = { 1, 2, 3, 4, 5 }; //строгая типизация
+
+            ArrayList arrayList = new ArrayList 
+            //->Проблема в том что тут может быть любой тип(object)
+            {
+                1,2,3,4,5,"",new object(), new File()
+            };
+
+
+            //Строгая типизация Т
+            List<int> numbersAsList = new List<int>
+            {
+                1,2,3,4,5
+            };
+        }
+    }
+}
